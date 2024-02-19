36b321df5d0a2e86bb8fc5e49f9f367036eca570
ItzzS1d <157032494+ItzzS1d@users.noreply.github.com>
2024-02-19 14:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const input = document.getElementById('input');
+
+
+input.onkeydown = (event) => {
+    if (event.key === 'Enter') {
+        alert("This is Myntra Clone Not a Real WebSite Thank You")
+}}
